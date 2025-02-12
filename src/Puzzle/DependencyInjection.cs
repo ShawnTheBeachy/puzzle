063185623fedf2cd79d3fa5c065b0cc39609f317
Shawn Beachy <shawnseanshaun@proton.me>
2025-02-12 15:17:55 -0500

    Add convenience method.

--- src/Puzzle/DependencyInjection.cs
+++ src/Puzzle/DependencyInjection.cs
@@ -1,10 +1,16 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Puzzle;
 
 public static class DependencyInjection
 {
+    public static IServiceCollection AddPlugins(
+        this IHostApplicationBuilder applicationBuilder,
+        Action<PuzzleConfiguration>? configure = null
+    ) => applicationBuilder.Services.AddPlugins(applicationBuilder.Configuration, configure);
+
     public static IServiceCollection AddPlugins(
         this IServiceCollection serviceCollection,
         IConfiguration configuration,
