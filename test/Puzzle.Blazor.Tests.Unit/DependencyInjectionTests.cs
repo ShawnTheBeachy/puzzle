7fa1ba87982340ed47d6843302687a58f464d442
Shawn Beachy <shawnseanshaun@proton.me>
2025-02-12 15:31:53 -0500

    Fix compile errors.

--- test/Puzzle.Blazor.Tests.Unit/DependencyInjectionTests.cs
+++ test/Puzzle.Blazor.Tests.Unit/DependencyInjectionTests.cs
@@ -37,11 +37,11 @@ public sealed class DependencyInjectionTests
         // Arrange.
         var typeProviderA = Substitute.For<ITypeProvider>();
         typeProviderA.GetTypes().Returns([typeof(ComponentA), typeof(string)]);
-        var pluginA = new Plugin(typeProviderA);
+        var pluginA = new Plugin(typeProviderA, null!);
 
         var typeProviderB = Substitute.For<ITypeProvider>();
         typeProviderB.GetTypes().Returns([typeof(ComponentB), typeof(int)]);
-        var pluginB = new Plugin(typeProviderB);
+        var pluginB = new Plugin(typeProviderB, null!);
 
         var services = new ServiceCollection();
         var config = new PuzzleConfiguration(
