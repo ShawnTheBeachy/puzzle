d6e4b295c1cd41b25627074404c0d8bc475c9022
Shawn Beachy <shawnseanshaun@proton.me>
2025-02-12 21:53:16 -0500

    Add AssemblyScanning.

---
+++ src/Puzzle/PluginOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Puzzle;
+
+internal sealed record PluginOptions
+{
+    public required IReadOnlyList<string> Locations { get; init; }
+    public const string SectionName = "Plugins";
+}
---
+++ test/Puzzle.Tests.Unit/AssemblyScanningTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Puzzle.Tests.Unit;
+
+public sealed class AssemblyScanningTests
+{
+    [Test]
+    public async Task ScanAssemblies_ShouldReturnAssembly_WhenItIsInLocationSpecifiedInOptions()
+    {
+        // Arrange.
+        var options = new PluginOptions
+        {
+            Locations =
+            [
+                new DirectoryInfo(
+                    new FileInfo(typeof(AssemblyScanningTests).Assembly.Location).DirectoryName!
+                )
+                    .Parent!
+                    .FullName,
+            ],
+        };
+
+        // Act.
+        var assemblies = AssemblyScanning.ScanAssemblies(options).ToArray();
+
+        // Assert.
+        await Assert
+            .That(assemblies)
+            .Contains(x => x.Location == typeof(AssemblyScanningTests).Assembly.Location);
+    }
+}
