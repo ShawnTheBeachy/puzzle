37f5966ed89f4671a5ac167f7bc9f72fb3bdc7c9
Shawn Beachy <shawnseanshaun@proton.me>
2025-02-12 21:43:22 -0500

    Add PluginLoadContext and covering tests.

---
+++ src/Puzzle/PluginLoadContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Puzzle;
+
+internal sealed class PluginLoadContext(string path) : AssemblyLoadContext
+{
+    private readonly AssemblyDependencyResolver _resolver = new(path);
+
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        var assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+        return assemblyPath is not null ? LoadFromAssemblyPath(assemblyPath) : null;
+    }
+
+    protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+    {
+        var libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+        return libraryPath is not null ? LoadUnmanagedDllFromPath(libraryPath) : IntPtr.Zero;
+    }
+}
