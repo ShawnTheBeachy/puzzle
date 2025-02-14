43f0789d39a078f3a57a2a8ee7bd8610a2807bff
Shawn Beachy <shawnseanshaun@proton.me>
2025-02-14 17:59:15 -0500

    Log warning when location does not exist while scanning assemblies.

--- src/Puzzle/Logging.cs
+++ src/Puzzle/Logging.cs
@@ -11,12 +11,16 @@ internal static partial class Logging
         string pluginId
     );
 
+    [LoggerMessage(LogLevel.Warning, Messages.PluginLocationNotExists)]
+    public static partial void PluginLocationNotExists(this ILogger logger, string location);
+
     [LoggerMessage(LogLevel.Warning, Messages.StartupThresholdWarning)]
     public static partial void StartupThresholdWarning(this ILogger logger, TimeSpan elapsed);
 
-    public static partial class Messages
+    public static class Messages
     {
         public const string DiscoveredPlugin = "Discovered plugin {Plugin} ({PluginId})";
+        public const string PluginLocationNotExists = "Plugin location {Location} does not exist";
         public const string StartupThresholdWarning = "Plugin initialization took {Elapsed}";
     }
 }
--- test/Puzzle.Tests.Unit/AssemblyScanningTests.cs
+++ test/Puzzle.Tests.Unit/AssemblyScanningTests.cs
@@ -1,9 +1,33 @@
+using Microsoft.Extensions.Logging;
 using Puzzle.Options;
 
 namespace Puzzle.Tests.Unit;
 
 public sealed class AssemblyScanningTests
 {
+    [Test]
+    public async Task ScanAssemblies_ShouldLogWarning_WhenLocationDoesNotExist()
+    {
+        // Arrange.
+        const string location = "/my/fake/path";
+        var options = new PuzzleOptions { Locations = [location, GlobalHooks.PluginsPath] };
+        var logger = new TestableLogger<AssemblyScanningTests>();
+
+        // Act.
+        _ = AssemblyScanning.ScanAssemblies(options, logger).ToArray();
+
+        // Assert.
+        using var asserts = Assert.Multiple();
+        await Assert.That(logger.LogMessages).HasCount().EqualToOne();
+        await Assert
+            .That(logger.LogMessages)
+            .Contains(x =>
+                x.LogLevel == LogLevel.Warning
+                && x.Message
+                    == Logging.Messages.PluginLocationNotExists.Replace("{Location}", location)
+            );
+    }
+
     [Test]
     public async Task ScanAssemblies_ShouldReturnAssembly_WhenItIsInLocationSpecifiedInOptions()
     {
@@ -21,7 +45,9 @@ public sealed class AssemblyScanningTests
         };
 
         // Act.
-        var assemblies = AssemblyScanning.ScanAssemblies(options).ToArray();
+        var assemblies = AssemblyScanning
+            .ScanAssemblies(options, Substitute.For<ILogger>())
+            .ToArray();
 
         // Assert.
         await Assert
@@ -33,10 +59,12 @@ public sealed class AssemblyScanningTests
     public async Task ScanAssemblies_ShouldSkipLocation_WhenItDoesNotExist()
     {
         // Arrange.
-        var options = new PuzzleOptions { Locations = ["/usr/plugins"] };
+        var options = new PuzzleOptions { Locations = ["/my/fake/path"] };
 
         // Act.
-        var assemblies = AssemblyScanning.ScanAssemblies(options).ToArray();
+        var assemblies = AssemblyScanning
+            .ScanAssemblies(options, Substitute.For<ILogger>())
+            .ToArray();
 
         // Assert.
         await Assert.That(assemblies).IsEmpty();
